973ab6ac7ecd343deca38bd042724923705d30da
Soo <soojlee0106@einsis.com>
2022-11-17 10:05:47 +0900

    Refactor code

--- movie.py
+++ movie.py
@@ -7,22 +7,6 @@ df2 = pd.read_csv('./tmdb_5000_movies.csv')
 df1.columns = ['id', 'tittle', 'cast', 'crew']
 df2 = df2.merge(df1, on='id')
 
-C = df2['vote_average'].mean()
-m = df2['vote_count'].quantile(0.9)
-q_movies = df2.copy().loc[df2['vote_count'] >= m]
-
-
-def weighted_rating(x, m=m, C=C):
-    v = x['vote_count']
-    R = x['vote_average']
-    return (v/(v+m) * R) + (m/(m+v) * C)
-
-
-q_movies['score'] = q_movies.apply(weighted_rating, axis=1)
-q_movies = q_movies.sort_values('score', ascending=False)
-
-pop = df2.sort_values('popularity', ascending=False)
-
 tfidf = TfidfVectorizer(stop_words='english')
 df2['overview'] = df2['overview'].fillna('')
 tfidf_matrix = tfidf.fit_transform(df2['overview'])
--- plot.py
+++ plot.py
@@ -21,10 +21,11 @@ q_movies['score'] = q_movies.apply(weighted_rating, axis=1)
 q_movies = q_movies.sort_values('score', ascending=False)
 
 pop = df2.sort_values('popularity', ascending=False)
-plt.figure(figsize=(12, 4))
 
-plt.barh(pop['title'].head(6), pop['popularity'].head(6), align='center',
-         color='skyblue')
+
+# Most popular movies according to IMDB ratings
+plt.figure(figsize=(12, 4))
+plt.barh(pop['title'].head(6), pop['popularity'].head(6), align='center')
 plt.gca().invert_yaxis()
 plt.xlabel("Popularity")
 plt.title("Popular Movies")
